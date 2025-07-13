46f92c080a065d3bac4f0e0772961231d9ddd97c
Cuong Tran <emsignouc0407@gmail.com>
2025-07-13 14:56:43 +0700

    chore: add comment to core.output code

--- src/core/output/PrintNumber.java
+++ src/core/output/PrintNumber.java
@@ -2,10 +2,12 @@ package core.output;
 
 public class PrintNumber {
     public static void main(String[] args) {
+        // Output numbers
         System.out.println(1);
         System.out.println(12);
         System.out.println(123);
 
+        // Output arithmetic operations
         System.out.println(1 + 1);
         System.out.println(2 * 2);
     }
--- src/core/output/PrintText.java
+++ src/core/output/PrintText.java
@@ -2,10 +2,12 @@ package core.output;
 
 public class PrintText {
     public static void main(String[] args) {
+        // Output text with a new line at the end
         System.out.println("Hello World!");
         System.out.println("I am learning Java!");
         System.out.println("It is awesome!");
 
+        // Output text without a new line at the end
         System.out.print("Hello World!");
         System.out.print("I am learning Java!");
         System.out.print("It is awesome!");
