a27461c7f5ee48a0aa3734be53defaa39c5b207c
Cuong Tran <emsignouc0407@gmail.com>
2025-07-13 19:35:20 +0700

    chore: complete core.strings

---
+++ src/core/strings/SpecialCharacters.java
@@ -0,0 +1,29 @@
+package core.strings;
+
+public class SpecialCharacters {
+    public static void main(String[] args) {
+        // Single quote
+        System.out.println("Hello! My name is \'Tom Tran\'.");
+
+        // Double quote
+        System.out.println("Hello! My name is \"Tom Tran\".");
+
+        // Backslash
+        System.out.println("Hello! My name is \\Tom Tran\\.");
+
+        // Newline
+        System.out.println("Hello!\nMy name is Tom Tran.");
+
+        // Carriage return
+        System.out.println("Hello!\rMy name is Tom Tran.");
+
+        // Tab
+        System.out.println("Hello!\tMy name is Tom Tran.");
+
+        // Backspace
+        System.out.println("Hello!\bMy name is Tom Tran.");
+
+        // Form feed
+        System.out.println("Hello!\fMy name is Tom Tran.");
+    }
+}
