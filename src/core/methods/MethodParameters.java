588cba0a1c27db13a370e23867441c6a375d14c5
Cuong Tran <emsignouc0407@gmail.com>
2025-07-14 13:15:52 +0700

    chore: complete core.methods

---
+++ src/core/methods/MethodParameters.java
@@ -0,0 +1,21 @@
+package core.methods;
+
+public class MethodParameters {
+    public static void mySingleMethod(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static void myMultipleMethod(String name, int age) {
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+    }
+
+    public static void main(String[] args) {
+        // Method with a single parameter
+        mySingleMethod("Cuong");
+        mySingleMethod("Tom");
+
+        // Method with multiple parameters
+        myMultipleMethod("Cuong", 18);
+        myMultipleMethod("Tom", 20);
+    }
+}
